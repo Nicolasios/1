5da6c1e09363a494cb53ad0d2f955162e8d31632
tracer-ics2020 <tracer@njuics.org>
2022-07-13 10:43:16 +0800

    >  compile U201814484 ty  10:43:16 up  2:04,  0 users,  load average: 0.09, 0.07, 0.01

--- nemu/src/utils/transfer.c
+++ nemu/src/utils/transfer.c
@@ -5,8 +5,10 @@ void char2int(char *c)
     Log("%s转换", c);
     int i = strlen(c);
     int j;
+    int res = 0;
     for (j = 0; j < i; j++)
     {
         printf("%d\n", c[j] - '0');
+        res += (c[j] - '0') * 10;
     }
 }
