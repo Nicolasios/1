88e37ef436e3b3f0ae9b541ca2110dc4186327dd
tracer-ics2020 <tracer@njuics.org>
2022-07-14 10:57:10 +0800

    >  compile U201814484 ty  10:57:10 up  2:29,  0 users,  load average: 0.07, 0.02, 0.00

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -163,16 +163,11 @@ static int cmd_x(char *args)
   //获取地址
   char *ptr;
   paddr_t begin_addr = strtol(EXPR, &ptr, 16);
+  printf("0x%08x:", begin_addr);
   int i;
   for (i = 0; i < n; i++, begin_addr += READ_BATCH)
   {
-    word_t data = paddr_read(begin_addr + i * READ_BATCH, READ_BATCH);
-    printf("0x%08x:", begin_addr + i * READ_BATCH);
-    for (int j = 0; j < READ_BATCH; j++)
-    {
-      printf("0x%02lx ", data & 0xff);
-      data = data >> 8;
-    }
+    printf("0x%08x:", paddr_read(begin_addr + i * READ_BATCH, READ_BATCH));
   }
 }
 
