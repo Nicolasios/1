f29d6ad1eff3397fb54be9eb69bb91e1e2bb99ab
tracer-ics2020 <tracer@njuics.org>
2022-07-21 16:40:44 +0800

    >  compile U201814484 ty  16:40:44 up  5:27,  0 users,  load average: 0.01, 0.04, 0.03

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -419,10 +419,10 @@ res_t eval(int p, int q)
     int op_type;
     int op = 0;
     getmainop(p, q, &op_type, &op);
-    // printf("main op p :%d,main op q :%d ,main op position:%d\n", p, q, op);
+    printf("main op p :%d,main op q :%d ,main op position:%d\n", p, q, op);
     res_t val1 = eval(p, op - 1);
     res_t val2 = eval(op + 1, q);
-    // printf("%ld %s %ld\n", val1, tokens[op].str, val2);
+    printf("%ld %s %ld\n", val1, tokens[op].str, val2);
     switch (op_type)
     {
     case TK_PLUS:
