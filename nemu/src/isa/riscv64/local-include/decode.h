0703c55614bafabe69a2781ed86d462632251634
tracer-ics2020 <tracer@njuics.org>
2022-07-27 14:16:59 +0800

    表达式求值新增运算符 U201814484-2022-7-15-PA1-2完成 ty  14:16:59 up  5:38,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv64/local-include/decode.h
+++ nemu/src/isa/riscv64/local-include/decode.h
@@ -3,16 +3,18 @@
 
 // decode operand helper
 #define def_DopHelper(name) \
-  void concat(decode_op_, name) (DecodeExecState *s, Operand *op, word_t val, bool load_val)
+  void concat(decode_op_, name)(DecodeExecState * s, Operand * op, word_t val, bool load_val)
 
-static inline def_DopHelper(i) {
+static inline def_DopHelper(i)
+{
   op->type = OP_TYPE_IMM;
   op->imm = val;
-
+  Log("def_Dop_i");
   print_Dop(op->str, OP_STR_SIZE, "%ld", op->imm);
 }
 
-static inline def_DopHelper(r) {
+static inline def_DopHelper(r)
+{
   op->type = OP_TYPE_REG;
   op->reg = val;
   op->preg = &reg_d(val);
@@ -20,20 +22,23 @@ static inline def_DopHelper(r) {
   print_Dop(op->str, OP_STR_SIZE, "%s", reg_name(op->reg));
 }
 
-static inline def_DHelper(I) {
+static inline def_DHelper(I)
+{
   decode_op_r(s, id_src1, s->isa.instr.i.rs1, true);
   decode_op_i(s, id_src2, (sword_t)s->isa.instr.i.simm11_0, true);
   decode_op_r(s, id_dest, s->isa.instr.i.rd, false);
 }
 
-static inline def_DHelper(U) {
+static inline def_DHelper(U)
+{
   decode_op_i(s, id_src1, (sword_t)s->isa.instr.u.simm31_12 << 12, true);
   decode_op_r(s, id_dest, s->isa.instr.u.rd, false);
 
   print_Dop(id_src1->str, OP_STR_SIZE, "0x%x", s->isa.instr.u.simm31_12);
 }
 
-static inline def_DHelper(S) {
+static inline def_DHelper(S)
+{
   decode_op_r(s, id_src1, s->isa.instr.s.rs1, true);
   sword_t simm = (s->isa.instr.s.simm11_5 << 5) | s->isa.instr.s.imm4_0;
   decode_op_i(s, id_src2, simm, true);
