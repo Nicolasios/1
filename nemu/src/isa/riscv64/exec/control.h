a733a8a78d3320b54839f4df12982fb6e74afafc
tracer-ics2020 <tracer@njuics.org>
2022-07-28 15:49:42 +0800

    表达式求值新增运算符 U201814484-2022-7-15-PA1-2完成 ty  15:49:42 up  7:10,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv64/exec/control.h
+++ nemu/src/isa/riscv64/exec/control.h
@@ -3,8 +3,8 @@ static inline def_EHelper(jal)
   //Log("sqe:0x%x jmp: 0x%x",s->seq_pc,s->jmp_pc);
   rtl_li(s, ddest,s->seq_pc);
   int offset = id_src1->imm;
-  Log("0x%lx   0x%lx",offset,offset>>20);
+  // Log("0x%lx   0x%lx",offset,offset>>20);
   rtl_j(s, offset>>20);
-  Log("sqe:0x%x jmp: 0x%x",s->seq_pc,s->jmp_pc);
+  Log("sqe:0x%x jmp: 0x%x  isJmp:%d",s->seq_pc,s->jmp_pc,s->is_jmp);
   print_asm_template2(jal);
 }
