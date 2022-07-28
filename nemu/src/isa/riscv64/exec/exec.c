c22ca2bfb864dfb93e4f44d9826c6d592d3f1c2c
tracer-ics2020 <tracer@njuics.org>
2022-07-28 16:45:04 +0800

    表达式求值新增运算符 U201814484-2022-7-15-PA1-2完成 ty  16:45:04 up  8:05,  0 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/isa/riscv64/exec/exec.c
+++ nemu/src/isa/riscv64/exec/exec.c
@@ -31,6 +31,7 @@ static inline def_EHelper(store)
 static inline void fetch_decode_exec(DecodeExecState *s)
 {
   s->isa.instr.val = instr_fetch(&s->seq_pc, 4);
+  Log("0x%lx",s->isa.instr.val);
   Assert(s->isa.instr.i.opcode1_0 == 0x3, "Invalid instruction");
   // Log("%d", s->isa.instr.i.opcode6_2);
   switch (s->isa.instr.i.opcode6_2)
