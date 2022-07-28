81eab6e0c5b2b84a36c4b74b8a222b40dd7d9f9f
tracer-ics2020 <tracer@njuics.org>
2022-07-28 16:46:00 +0800

    表达式求值新增运算符 U201814484-2022-7-15-PA1-2完成 ty  16:46:00 up  8:06,  0 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/isa/riscv64/exec/compute.h
+++ nemu/src/isa/riscv64/exec/compute.h
@@ -7,7 +7,7 @@ static inline def_EHelper(lui)
 //U
 static inline def_EHelper(auipc)
 {
-  Log("0x%lx",id_src1->imm);
+  // Log("0x%lx",id_src1->imm);
   rtl_li(s, ddest, id_src1->imm + s->seq_pc - 4 );
   print_asm_template2(auipc);
 }
