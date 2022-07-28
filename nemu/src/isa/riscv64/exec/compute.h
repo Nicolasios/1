4333979c5f034630cd60180b9b76c9ce33cccc1e
tracer-ics2020 <tracer@njuics.org>
2022-07-28 14:59:41 +0800

    表达式求值新增运算符 U201814484-2022-7-15-PA1-2完成 ty  14:59:41 up  6:20,  0 users,  load average: 0.08, 0.02, 0.01

--- nemu/src/isa/riscv64/exec/compute.h
+++ nemu/src/isa/riscv64/exec/compute.h
@@ -1,17 +1,18 @@
+//U
 static inline def_EHelper(lui)
 {
   rtl_li(s, ddest, id_src1->imm);
   print_asm_template2(lui);
 }
-
+//U
 static inline def_EHelper(auipc)
 {
-  rtl_addi(s, ddest, &s->seq_pc, id_src1->imm);
+  rtl_li(s, ddest, id_src1->imm);
   print_asm_template2(auipc);
 }
-
+//I
 static inline def_EHelper(addi)
 {
-  rtl_addi(s, ddest, id_src1, id_src2->imm);
+  rtl_addi(s, ddest, dsrc1, id_src2->imm);
   print_asm_template2(addi);
 }
--- nemu/src/isa/riscv64/exec/control.h
+++ nemu/src/isa/riscv64/exec/control.h
@@ -1,5 +1,6 @@
 static inline def_EHelper(jal)
 {
-  rtl_li(s, ddest, id_src1->imm);
+  rtl_li(s, ddest,s->seq_pc);
+  rtl_j(s,c_sar(id_src1->imm,12));
   print_asm_template2(jal);
 }
